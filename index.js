88e0a2b6bfa1ca2881b6f53d6369c36b29f55f20
Paul Hsueh <hsueh.paul@gmail.com>
2021-04-16 11:07:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.body.remove('main');
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1> is the champion </h1>';
